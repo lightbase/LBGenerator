f640ff4db9bab8ee4ee3c44af7691913ae13b59b
antonygc91 <antonygc91@gmail.com>
2014-08-26 14:10:26 -0300

    es query interface

--- lbgenerator/views/es.py
+++ lbgenerator/views/es.py
@@ -3,6 +3,9 @@ from pyramid.response import Response
 from pyramid.exceptions import HTTPNotFound
 import requests
 from . import CustomView
+from ..model.context.document import DocumentContextFactory
+from .document import DocumentCustomView
+from ..lib.utils import FakeRequest
 
 class ESCustomView(CustomView):
 
@@ -11,20 +14,58 @@ class ESCustomView(CustomView):
         self.request = request
         self.context.base_name = self.request.matchdict['base']
 
+    @staticmethod
+    def map_id_doc(es_hits):
+        return es_hits['fields']['_metadata.id_doc'][0]
+
     def get_interface(self):
         params = dict(self.request.params)
+
+        if 'lbquery' in params:
+            params.pop('lbquery')
+            query_lb = True
+        else:
+            query_lb = False
+
         url = self.context.get_base().metadata.idx_exp_url
         path = self.request.matchdict['path']
         if path:
             url += path
         response = requests.get(url, params=params)
+
+        if query_lb:
+            response_json = response.json()
+            id_docs = repr(tuple(map(self.map_id_doc, response_json['hits']['hits'])))
+            mock_request = FakeRequest(
+                params = {'$$': '{"literal":"id_doc in %s", "limit":null}}' % (id_docs)},
+                matchdict = {'base': self.request.matchdict['base']})
+            doc_factory = DocumentContextFactory(mock_request)
+            doc_view = DocumentCustomView(doc_factory, mock_request)
+            return doc_view.get_collection()
+
         return Response(response.text, content_type='application/json')
 
     def post_interface(self):
         url = self.context.get_base().metadata.idx_exp_url
         params = dict(self.request.params)
+        if 'lbquery' in params:
+            params.pop('lbquery')
+            query_lb = True
+        else:
+            query_lb = False
         path = self.request.matchdict['path']
         if path:
             url += path
         response = requests.get(url, params=params, data=self.request.body)
+
+        if query_lb:
+            response_json = response.json()
+            id_docs = repr(tuple(map(self.map_id_doc, response_json['hits']['hits'])))
+            mock_request = FakeRequest(
+                params = {'$$': '{"literal":"id_doc in %s", "limit":null}}' % (id_docs)},
+                matchdict = {'base': self.request.matchdict['base']})
+            doc_factory = DocumentContextFactory(mock_request)
+            doc_view = DocumentCustomView(doc_factory, mock_request)
+            return doc_view.get_collection()
+
         return Response(response.text, content_type='application/json')
