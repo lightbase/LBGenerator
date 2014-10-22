09e93a88afc124b5f93211aad813da63cfcff078
edulucio <eduardo.lucio@lightbase.com.br>
2014-10-22 17:31:16 -0200

    Ajustes no ES! By Questor

--- lbgenerator/views/es.py
+++ lbgenerator/views/es.py
@@ -90,9 +90,10 @@ class ESCustomView(CustomView):
                 matchdict = {'base': self.request.matchdict['base']})
             doc_factory = DocumentContextFactory(mock_request)
             doc_view = DocumentCustomView(doc_factory, mock_request)
+            return Response(str(dir(doc_view)) + str(dir(doc_view.get_collection())))
             doc_view_get_collection = doc_view.get_collection()
-            doc_view_get_collection['offset'] = offset
-            doc_view_get_collection['limit'] = limit
+            # doc_view_get_collection['offset'] = offset
+            # doc_view_get_collection['limit'] = limit
             return doc_view_get_collection
 
         return Response(response.text, content_type='application/json')
