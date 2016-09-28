55630558c65430f4b9cf13dc02d6106916a273d2
Danilo Carvalho <dfcarvalho85@gmail.com>
2016-09-28 14:27:20 -0300

    Funcionalidade #2412 - Integrar os esquemas e funcionalidades integradores e facilitadoras do uso do ES ao LBG - LBGenerator
    Acrescentadas opções à rota /{base}/lbes (Busca Textual via ElasticSearch):
     - "from" e "size": offset
     - search_fields
     - result_fields
     - sort
     - raw_es_response
    
    Alteração do formato do retorno padrão para um formato mais simples em vez do retorno normal do ES.

--- lbgenerator/views/lbes.py
+++ lbgenerator/views/lbes.py
@@ -3,6 +3,7 @@ import requests
 import collections
 
 from pyramid.response import Response
+from pyramid.httpexceptions import HTTPBadRequest
 
 from ..model.context.es import ESContextFactory
 from liblightbase.lbutils.codecs import json2object
@@ -19,25 +20,56 @@ class LBSearch:
     def __call__(self):
         return self.execute_es_query()
 
-
     def execute_es_query(self):
-        # parse json body
         dict_request = self.request.json_body
 
-        in_source = False
+        if 'query' not in dict_request:
+            return HTTPBadRequest(body="No 'query' field")
+
+        # default fields
+        raw_es_response = False
+        hl_in_source = False
+        offset_from = 0
+        offset_size = 10
+        search_fields = ['_all']
+        sort = ['_score']
+
+        # override default fields
+        if 'raw_es_response' in dict_request:
+            raw_es_response = dict_request['raw_es_response']
+
+        if 'search_fields' in dict_request:
+            search_fields = dict_request['search_fields']
+
+        if 'from' in dict_request:
+            offset_from = dict_request['from']
+
+        if 'size' in dict_request:
+            offset_size = dict_request['size']
+
+        if 'sort' in dict_request:
+            sort = dict_request['sort']
+            if '_score' not in sort:
+                sort.append('_score')
         
         # TODO: validate fields
 
         # create es query
         dict_es_query = {
+            'from': offset_from,
+            'size': offset_size,
             'query': {
                 'query_string': {
                     'default_operator': "AND",
                     'query': dict_request['query'],
-                    'fields': ['_all']
+                    'fields': search_fields
                 }
-            }
+            },
+            'sort': sort
         }
+
+        if 'return_fields' in dict_request:
+            dict_es_query['_source'] = dict_request['return_fields']
 
         if 'highlight' in dict_request:
             self.highlight_req = dict_request['highlight']
@@ -45,45 +77,46 @@ class LBSearch:
             dict_es_query['highlight'] = {
                 'fields': {
                     '*': {
-                        'number_of_fragments': 10,
-                        'fragment_size': 500
+                        'number_of_fragments': 30,
+                        'fragment_size': 1
                     }
                 }
             }
 
-            in_source = dict_request['highlight'].pop('in_source', False)
+            hl_in_source = dict_request['highlight'].pop('in_source', False)
 
             dict_es_query['highlight'].update(dict_request['highlight'])
 
 
-        # if 'override' in dict_request:
-        #     dict_es_query = _update_es_query(dict_es_query, dict_request['override'])
-
-        # BEGIN DEBUG
-        print(str(dict_es_query))
-        # END DEBUG
-        url = self.context.get_base().metadata.idx_exp_url
-        # path = self.request.matchdict['path']
-        # if path: 
-            # url += path
-        # else:
-        url += '/_search'
+        url = self.context.get_base().metadata.idx_exp_url + '/_search'
 
         json_es_query = json.dumps(dict_es_query)
         response = requests.get(url, data=json_es_query)
         response_text = response.text
 
-        if in_source:
-            dict_response = json.loads(response.text)
+        if hl_in_source:
+            dict_response = json.loads(response_text)
             if dict_response['hits']['total'] > 0:
                 hits = dict_response['hits']['hits']
                 for hit in hits:
                     if 'highlight' in hit:
                         for k, v in hit['highlight'].items():
                             self._update_highlight(hit['_source'], k.split('.'), v)
-                    hit.pop('highlight')
-            response_text = json.dumps(dict_response)
+                        hit.pop('highlight')
+                response_text = json.dumps(dict_response)
 
+        if not raw_es_response:
+            dict_response = json.loads(response_text)
+            new_dict_response = list()
+            hits = dict_response['hits']['hits']
+            for hit in hits:
+                new_hit = dict()
+                new_hit['data'] = hit['_source']
+                new_hit['score'] = hit['_score']
+                new_dict_response.append(new_hit)
+                if not hl_in_source and 'highlight' in hit:
+                    new_hit['highlight'] = hit['highlight']
+            response_text = json.dumps(new_dict_response)
 
         return Response(response_text, content_type='application/json')
 
@@ -101,12 +134,11 @@ class LBSearch:
                 for hl_value in hl_values:
                     no_hl_value = self._get_no_highlight_value(hl_value)
 
-                    if source[key] == no_hl_value:
-                        source[key] = hl_value
-                        hl_values.remove(hl_value)
-                        break
+                    if key in source and no_hl_value in source[key]:
+                        source[key] = source[key].replace(no_hl_value, hl_value)
         else:
-            self._update_highlight(source[key], l_path, hl_values)
+            if key in source:
+                self._update_highlight(source[key], l_path, hl_values)
             
 
     def _get_no_highlight_value(self, value):
